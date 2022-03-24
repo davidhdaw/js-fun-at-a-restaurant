3806cbe5ddb1ddfd386ddee222797f89938864bc
David Daw <daviddaw@gmail.com>
2022-03-24 10:10:57 -0700

    Add first 3 meal functions

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,29 @@
-function nameMenuItem() {
+function nameMenuItem(food) {
+  return `Delicious ${food}`
+};
 
-}
+function createMenuItem(name, price, type) {
+  return {
+    name: name,
+    price: price,
+    type: type,
+  }
+};
+
+function addIngredients(ingredient, array) {
+    if (array.includes(ingredient) === true) {
+
+    } else {
+    array.push(ingredient);
+    }
+};
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
+  createMenuItem,
+  addIngredients,
   // formatPrice,
   // decreasePrice,
   // createRecipe
 }
-
-
