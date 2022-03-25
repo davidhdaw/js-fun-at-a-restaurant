10e4fcb918c4fa37d596a325d1ac6b46f51a3400
David Daw <daviddaw@gmail.com>
2022-03-24 19:18:07 -0700

    Add createRestaurant and some addMenuItem functionality.

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,28 @@
+var pizzaRestaurant = {
+  name: 'placeholder',
+
+}
+
+function createRestaurant(name) {
+  return {
+    name: name,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: [],
+    },
+  }
+};
+
+function addMenuItem(restaurant, item) {
+if (item.type === 'breakfast') {
+  restaurant.menus.breakfast.push(item);
+} else if ()
+};
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
+  createRestaurant,
+  addMenuItem,
   // removeMenuItem
 }
