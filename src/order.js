8ca25a80df087befe42453982668e7b12744c54f
David Daw <daviddaw@gmail.com>
2022-03-24 17:17:43 -0700

    Add order functions

--- src/order.js
+++ src/order.js
@@ -1,7 +1,42 @@
+var deliveryOrders = [];
+function takeOrder(order, array) {
+ if (array.length < 3) {
+   array.push(order);
+ } else {}
+};
+
+function refundOrder(orderNumber, array) {
+  for (i = 0; i < array.length; i++) {
+    if (orderNumber === array[i].orderNumber) {
+      array.splice(i, 1);
+    } else {}
+  }
+};
+
+function listItems(array) {
+  var string = array[0].item;
+for (i = 1; i < array.length; i++) {
+  string = string + ", " + array[i].item;
+}
+return string;
+};
+
+
+
+function searchOrder(array, item) {
+var match
+  for (i = 0; i < array.length; i++) {
+    if (item === array[i].item) {
+      return true;
+    } else {match = false;}
+  }
+  return match
+};
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
