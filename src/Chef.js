a22e28fbfaccc4de09a97a574481d3fc35e2534c
David Daw <daviddaw@gmail.com>
2022-03-26 14:29:50 -0700

    Add Chef class that passes all unit tests

--- src/Chef.js
+++ src/Chef.js
@@ -3,13 +3,22 @@ class Chef {
     this.name = name;
     this.restaurant = restaurant;
   };
+  
   greetCustomer(name, morning){
     if (morning === true) {
       return `Good morning, ${name}!`;
     } else {
       return `Hello, ${name}!`
+    }
+  };
+
+  checkForFood(foodItem){
+    if (this.restaurant.menus[foodItem.type].includes(foodItem)) {
+      return `Yes, we're serving ${foodItem.name} today!`
+    } else {
+      return `Sorry, we aren't serving ${foodItem.name} today.`
     };
   };
-}
+};
 
 module.exports = Chef;
